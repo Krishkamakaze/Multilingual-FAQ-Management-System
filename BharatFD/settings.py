fa98e76600e6cde2e45cc86c6d28dc747afacb5e
Krishkamakaze <krishkumar901255@gmail.com>
2025-02-02 18:46:36 +0530

    settings

--- BharatFD/settings.py
+++ BharatFD/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # 
-SECRET_KEY = 'django-insecure-06b#cym8h27cn)%m)9l893+qhy(r+mxauzctv!(jefbestc@7d'
+# SECRET_KEY = 'django-insecure-06b#cym8h27cn)%m)9l893+qhy(r+mxauzctv!(jefbestc@7d'
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
+# DEBUG = True
 
 
 
