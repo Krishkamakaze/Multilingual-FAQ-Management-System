9fc2b6f27885af91a252aa8d183c31efbb17d32e
Krishkamakaze <krishkumar901255@gmail.com>
2025-02-02 19:10:04 +0530

    change of basedir

--- BharatFD/settings.py
+++ BharatFD/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
